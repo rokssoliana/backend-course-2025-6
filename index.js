ef622c6ada6b7846adc8c9fefa9a6a1aa81325f6
Роксоляна bc2025-6 <roksasm@gmail.com>
2025-11-10 17:26:35 +0200

    chore: installed commander, superagent, nodemon and added .gitignore

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Server is starting...");
+import { Command } from "commander";
+const program = new Command();
+
+program
+  .requiredOption("-h, --host <host>", "server host address")
+  .requiredOption("-p, --port <port>", "server port")
+  .requiredOption("-c, --cache <path>", "path to cache directory");
+
+program.parse(process.argv);
+
+const options = program.opts();
+console.log("Server options:", options);
